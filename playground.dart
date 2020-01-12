a5c5d97131ed42f056150ac6bfae7c156e430f3a
olaniyi <adedoyin058@gmail.com>
2020-01-12 14:54:29 -0800

    added playground

--- playground.dart
+++ playground.dart
@@ -1,6 +1,6 @@
 
 main(){
-  List names = ['jack', 'jill', 'tola','alice'];
+  var names = ['jack', 'jill', 'tola','alice', 10, 100, 100.28];
     print(names.length);
     names[0] = 'tunde';
     for(var n in names){
