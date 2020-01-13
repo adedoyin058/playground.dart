0de46e6418a73f181c75b0f2824443c8f6b468c5
olaniyi <adedoyin058@gmail.com>
2020-01-13 15:04:56 -0800

    updated playground

--- playground.dart
+++ playground.dart
@@ -1,75 +1,32 @@
 // random names for establising a company
-import 'dart:io';
+//import 'dart:io';
 
-main(){
-   
-     var lst = new List(8);
-      lst [0]= 'adedoyin&co \n'; 
-      lst [1]= 'Goodtime company \n';
-      lst [2]= 'Boomerang \n';
-      lst [3]= 'Solar.ng \n';
-      lst [4]= 'Adedoyin.ng \n';
-      lst [5]= 'Ade.ng \n';
-      lst[6] = 12;
-      lst [7]= 34;
-      {
+//import 'dart:math';
 
-   
-print(lst);
-print('Please choose from the names listed above  to name your company');
-print('');
-   print('supply the name you will like to name your company');
-   
-  
-  var input = stdin.readLineSync();
-     print('The name of your company now is:  '+''+'$input'.toUpperCase());
-     
-   }
-   print(lst.length);
-   //var input = stdin.readLineSync();
-   //print('The total addition is:  '+''+'$input'.toUpperCase());
-   
-   //print('The total addition is:  '+''+'$input'.toUpperCase());
- // print(input.toUpperCase());
+ void  print_alphabet()
+{
+  var c = "a".codeUnits.first;
+  var i = 0;
+
+  while (++i <= 26)
+  {
+    print(c.toString());
+    c++;
+  }
+}
+
+void main() {
+  print_alphabet();
+
+  int c = "a".codeUnitAt(0);
+int end = "z".codeUnitAt(0);
+while (c <= end) {
+  print(new String.fromCharCode(c));
+  c++;
+}
+int char_a = 'a'.codeUnitAt(0);
+print(new String.fromCharCodes(new Iterable.generate(26, (x) => char_a + x)));
+
 
 }
-  // var n1= 'Paritie Dynamic';   
- //var n2= 'Goodtime company'; 
- //var n3= 'Adeddoyin&co'; 
 
-
-//print ('What name will you like to name your company? You can choose from the options below ==>');
-
-
-//print(n1);
-//print(n2);
-//print(n3);
-//print('Choose from the names given above");
-//if  ( n1 == 'Paritie Dynamic' )
-//{
-  //print('');
-  //print('Congratulations!!! Now your company name will be Paritie Dynamic');
-
-
-   //}
-   
-     //if (  n2 == n2)
-//{
-  //print('');
- //print('Congratulations!!! Now your company name will be Goodtime company');
-
-
-   // }
-  
- 
-       //else  (n3 == 'Adeddoyin&co') ;
-
-  //print('');
-  //print('Congratulations!!! Now your company name will be Adeddoyin&co');
-
-
-//print('');
-  // print('The name of your company is'+ '' +' $n1');
-
-
- 
