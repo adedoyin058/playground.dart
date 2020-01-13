7602a9d7d2d9201a4ce2d057598a9393626f9772
olaniyi <adedoyin058@gmail.com>
2020-01-13 06:03:42 -0800

    updated playground

--- playground.dart
+++ playground.dart
@@ -2,22 +2,33 @@
 import 'dart:io';
 main(){
    
-     var lst = new List(10);
-      lst [0]= 'adedoyin&co \n';
+     var lst = new List(8);
+      lst [0]= 'adedoyin&co \n'; 
       lst [1]= 'Goodtime company \n';
       lst [2]= 'Boomerang \n';
       lst [3]= 'Solar.ng \n';
       lst [4]= 'Adedoyin.ng \n';
       lst [5]= 'Ade.ng \n';
+      lst[6] = 12;
+      lst [7]= 34;
+      {
 
+   
 print(lst);
 print('Please choose from the names listed above  to name your company');
 print('');
    print('supply the name you will like to name your company');
+   
   
   var input = stdin.readLineSync();
      print('The name of your company now is:  '+''+'$input'.toUpperCase());
-  //print(input.toUpperCase());
+     
+   }
+   //var input = stdin.readLineSync();
+   //print('The total addition is:  '+''+'$input'.toUpperCase());
+   
+   //print('The total addition is:  '+''+'$input'.toUpperCase());
+ // print(input.toUpperCase());
 
 }
   // var n1= 'Paritie Dynamic';   
