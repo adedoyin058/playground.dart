444d4d4be7441f7bc0df4a767b7beff48a478f86
olaniyi <adedoyin058@gmail.com>
2020-01-13 01:41:13 -0800

    added playground

--- playground.dart
+++ playground.dart
@@ -1,8 +1,38 @@
-
+// random names for establising a company
 main(){
-  var gifts = Map();
-  gifts ['first'] = 'orange';
-      print(gifts ['first']);
+  var n1= 'Paritie Dynamic';   
+  var n2= 'Goodtime company'; 
+  var n3= 'Adeddoyin&co'; 
+
+
+print ('What name will you like to name your company? You can choose from the options below ==>');
+
+
+print(n1);
+print(n2);
+print(n3);
+//print('Choose from the names given above");
+if (n1 == 'Paritie Dynamic' )
+{
+  print('');
+  print('Congratulations!!! Now your company name will be Paritie Dynamic');
+
+   }
+
+    else if (n2 =='Paritie Dynamic')
+{
+  print('');
+  print('Congratulations!!! Now your company name will be Goodtime company');
+
+
     }
- 
   
+ 
+       else if (n2 == 'Paritie Dynamic')
+{
+  print('');
+  print('Congratulations!!! Now your company name will be Adeddoyin&co');
+
+}
+ print(n1);
+}
