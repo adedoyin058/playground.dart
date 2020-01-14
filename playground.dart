1ea6e92219c99a44f60f8928b17707a875439f83
olaniyi <adedoyin058@gmail.com>
2020-01-14 15:43:59 +0100

    added playground

--- playground.dart
+++ playground.dart
@@ -1,32 +1,18 @@
 // random names for establising a company
-//import 'dart:io';
 
-//import 'dart:math';
 
- void  print_alphabet()
-{
-  var c = "a".codeUnits.first;
-  var i = 0;
+import 'dart:core';
+//import 'package:random_string/random_string.dart';
 
-  while (++i <= 26)
-  {
-    print(c.toString());
-    c++;
-  }
+void main(){
+ 
+  StringBuffer sb = new StringBuffer();
+  sb.write("Hello");
+  sb.writeAll(['space \d ', 'and \d ', 'more \n']);
+ 
+  print(sb);
+  print(sb.toString());
+  
+  sb.clear();
 }
-
-void main() {
-  print_alphabet();
-
-  int c = "a".codeUnitAt(0);
-int end = "z".codeUnitAt(0);
-while (c <= end) {
-  print(new String.fromCharCode(c));
-  c++;
-}
-int char_a = 'a'.codeUnitAt(0);
-print(new String.fromCharCodes(new Iterable.generate(26, (x) => char_a + x)));
-
-
-}
-
+       
