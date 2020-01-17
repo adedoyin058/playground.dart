514733c88d266e07c772e60b5a8d2bef3b8e7703
olaniyi <adedoyin058@gmail.com>
2020-01-17 10:50:38 +0100

    modified playground

--- playground.dart
+++ playground.dart
@@ -1,21 +1,11 @@
-
+//import 'dart:io';
 import 'dart:core';
-main(){
-     div(x,y){
-      return x ~/ y;
-     }
-  try{
+//import 'dart:io';
+//import 'dart:math';
 
-    print(div(2,1));
-        print(div(2,0));
-        print(div(3,1));
-    
-      }
-      catch( Exception,stacktrace){
-        print( Exception);
-     print(stacktrace);
-      }
-      print("still working");
-    }
-    
-    
+main(){
+  int  D =56,d = 47;
+  
+  double A= D * d / 2;
+  print (A);
+}
